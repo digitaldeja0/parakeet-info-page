1d63c25573907551ab2c952ef44739917f0cf4a0
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2021-12-21 17:38:19 -0500

    Update index.js
    Update image location source. Add paragraph filler text.

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const root = document.getElementById("root")
 
 const myApp = (
     <div>
-         <img src="/parakeet-info-page/img/img1.jpg" alt="Parakeet" width="200px"></img>
+        <div>
+         <img src="/img/img1.jpg" alt="Parakeet" width="200px"></img>
+         </div>
+         <div>
+         <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+         </p>
+         <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+         </p>
+         <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+         </p>
+         <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+         </p>
+         </div>
     </div>
 )
 
