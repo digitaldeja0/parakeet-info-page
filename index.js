1a528c895191f8153205a1faaef0146dcf980da3
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2021-12-21 18:03:06 -0500

    Create app component
    Create component for homepage information.

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 
 const root = document.getElementById("root")
 
-
-const myApp = (
-    <div>
+// This is how you create a react componet.
+function MyApp(){
+    return (
         <div>
-         <img src="/img/img1.jpg" alt="Parakeet" width="200px"></img>
-         </div>
-         <div>
-         <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-         </p>
-         <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-         </p>
-         <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-         </p>
-         <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-         Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-         </p>
-         </div>
-    </div>
-)
 
-ReactDOM.render(myApp, root)
+        <header>
+            <h1> The Parakeet Page</h1>
+        </header>
+
+         <section>
+
+            <div>
+                <img src="/img/img1.jpg" alt="Parakeet" width="200px"></img>
+            </div>
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+                </p>
+                <p>
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+                </p>
+            </div>
+                
+         </section>
+
+         <footer>
+             <small>
+                    © 2022 Hey Deja
+             </small>
+         </footer>
+
+    </div>
+
+    )
+}
+
+ReactDOM.render(<MyApp />, root)
 
 
