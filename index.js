7c5e75556ce9d1a20955af57efa5f1fabebd2755
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2021-12-21 18:08:42 -0500

    Create parent-child components
    Create parent-child components.

--- index.js
+++ index.js
@@ -2,45 +2,73 @@
 const root = document.getElementById("root")
 
 // This is how you create a react componet.
+
+function Header () {
+    return (
+        <header>
+            <h1> The Parakeet Page</h1>
+        </header>
+    )
+
+}
+
+function Main () {
+    return (
+        <section>
+
+        <div>
+            <img src="/img/img1.jpg" alt="Parakeet" width="200px"></img>
+        </div>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+            </p>
+            <p>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
+            </p>
+        </div>
+            
+     </section>
+    )
+
+}
+
+
+
+
+
+function Footer () {
+    return (
+        <footer>
+        <small>
+               © 2022 Hey Deja
+        </small>
+    </footer>
+    )
+
+}
+
+
 function MyApp(){
     return (
         <div>
 
-        <header>
-            <h1> The Parakeet Page</h1>
-        </header>
+         <Header />
 
-         <section>
+         <Main />
 
-            <div>
-                <img src="/img/img1.jpg" alt="Parakeet" width="200px"></img>
-            </div>
-            <div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-                </p>
-                <p>
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-                    Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
-                </p>
-            </div>
-                
-         </section>
+         <Footer />
 
-         <footer>
-             <small>
-                    © 2022 Hey Deja
-             </small>
-         </footer>
 
     </div>
 
