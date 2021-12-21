292521c72b559261eda67839f2dc60ef78a7acb7
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2021-12-21 17:28:15 -0500

    Add image
    Add parakeet picture to homepage.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const root = document.getElementById("root")
 
 const myApp = (
     <div>
-
+         <img src="/parakeet-info-page/img/img1.jpg" alt="Parakeet" width="200px"></img>
     </div>
 )
 
