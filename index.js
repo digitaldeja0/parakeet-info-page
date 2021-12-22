0f42b7ec93a5f76d41f21476a95a3ce41a00231f
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2021-12-21 21:26:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
 const root = document.getElementById("root")
 
+
+
 // This is how you create a react componet.
 
 function Header () {
     return (
         <header className="header" >
             <h1> The Parakeet Page</h1>
+            {/* <div className="hamburger-div" >
+                <div className="hamburger"></div>
+                <div className="hamburger"></div>
+                <div className="hamburger"></div>
+            </div> */}
+            
             <nav className="nav-bar">
                 <ul className="nav-list" >
                     <li className="nav-item"> Home </li>
@@ -16,17 +24,26 @@ function Header () {
             </nav>
         </header>
     )
-
 }
+
 
 function Main () {
     return (
-        <section>
+        <section> 
 
-        <div>
-            <img src="/img/img1.jpg" alt="Parakeet" width="200px"></img>
+        {/* <div>
+                <ul className="nav-list2" id="navlist2" >
+                    <li className="nav-item2"> Home </li>
+                    <li className="nav-item2"> About Parakeets </li>
+                    <li className="nav-item2"> Parakeet Photos </li>
+                </ul>
+        </div> */}
+
+        <div className="hero-section" >
+            <img className="hero-img" src="/img/img1.jpg" alt="Parakeet"></img>
         </div>
-        <div>
+
+        <div className="intro" >
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
                 Quisque lacinia lectus sed massa tincidunt, at ullamcorper nulla viverra.
@@ -56,7 +73,7 @@ function Main () {
 
 function Footer () {
     return (
-        <footer>
+        <footer className="footer" >
         <small>
                © 2022 Hey Deja
         </small>
